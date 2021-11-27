56048f9d852ffd55417037a1f899eed17d797388
Juan Sebastian Higuita <juansehiguita@gmail.com>
2021-11-27 01:56:43 -0500

    Visualizacion de los puntos en el mapa

--- html/index.js
+++ html/index.js
@@ -1,24 +1,27 @@
 function initMap(data) {
     var lat = data[0]['Latitud'].replace(',', '.');
     var lng = data[0]['Longitud'].replace(',', '.');
+    
     var center = {lat: parseFloat(lat), lng: parseFloat(lng)};
-    console.log(center);
     var map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
         zoom: 9,
-        center: center
     });
     var infowindow =  new google.maps.InfoWindow({});
     var marker;
-    
     data.forEach(e => {
+        lat = parseFloat(e[`Latitud`].replace(',', '.'));
+        lng = parseFloat(e[`Longitud`].replace(',', '.'));
+        //console.log(lat,",",lng);
         marker = new google.maps.Marker({
-        position: new google.maps.LatLng(parseFloat(e[`Latitud`].replace(',', '.')), parseFloat(e[`Longitud`].replace(',', '.'))),
+        position: new google.maps.LatLng(lat,lng),
         map: map,
         title: e[`Direccion_incidente`],
         });
         google.maps.event.addListener(marker, 'click', (function (marker) {
             return function () {
-                infowindow.setContent(`Barrio: ${e['Barrio']}<br>Comuna: ${e['Comuna']}<br>Direccion: ${e['Direccion_incidente']}<br>Gravedad: ${e['Gravedad_victima']}<br>Condicion: ${e['Condicion']}<br>Sexo: ${e['Sexo']}<br>Grupo Edad: ${e['Grupo_edad']}`);
+                //infowindow.setContent(`Cluster: ${e['Kmeans_clusters']}<br>Barrio: ${e['Barrio']}<br>Comuna: ${e['Comuna']}<br>Direccion: ${e['Direccion_incidente']}<br>Gravedad: ${e['Gravedad_victima']}<br>Condicion: ${e['Condicion']}<br>Sexo: ${e['Sexo']}<br>Grupo Edad: ${e['Grupo_edad']}`);
+                infowindow.setContent(`Cluster: ${e['Kmeans_clusters']}<br>Gravedad: ${e['Gravedad_victima']}<br>Condicion: ${e['Condicion']}<br>Sexo: ${e['Sexo']}<br>Grupo Edad: ${e['Grupo_edad']}`);
                 infowindow.open(map, marker);
             }
         })(marker));
--- processdata.py
+++ processdata.py
@@ -1,8 +1,21 @@
-# Importing the pandas package.
+#Importing the pandas package.
 import pandas as pd
 def processdata():
     # Loading and reading the CSV file.
-    df = pd.read_csv('AccidentesShortTextoWithClusters.csv', sep = ';', nrows = 1000)
+    df = pd.read_csv('dataframeNormalizado.csv', sep = ';',nrows = 1000)
+    
     # Converting the CSV data to a list.
     df = df.to_dict(orient = 'records')
     return df
+"""
+import chardet
+import pandas as pd
+
+def processdata():
+    with open('dataframeNormalizado.csv', 'rb') as f:
+        result = chardet.detect(f.read())  # or readline if the file is large
+    df = pd.read_csv('dataframeNormalizado.csv', sep = ';', encoding=result['encoding'])
+    # Converting the CSV data to a list.
+    df = df.to_dict(orient = 'records')
+    return df
+"""
