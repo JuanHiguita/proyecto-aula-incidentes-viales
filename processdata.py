1557be5ad39d2bb798a6e84bb074cab07052fb98
juanseHiguita <juansehiguita@gmail.com>
2021-11-27 20:44:50 -0500

    Arreglos en la ortografia, y version final

--- processdata.py
+++ processdata.py
@@ -3,7 +3,6 @@ import pandas as pd
 def processdata():
     # Loading and reading the CSV file.
     df = pd.read_csv('dataframeNormalizado.csv', sep = ';',nrows = 1000)
-    
     # Converting the CSV data to a list.
     df = df.to_dict(orient = 'records')
     return df
